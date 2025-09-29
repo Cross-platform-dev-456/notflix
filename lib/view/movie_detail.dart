e0c5ade07e128e105a56b14c3c7f70cd2c5b2ef9
Landen Tomlin <145607823+tomlinl1@users.noreply.github.com>
2025-09-29 14:08:40 -0400

    Added styling and font changes to detail page

--- lib/view/movie_detail.dart
+++ lib/view/movie_detail.dart
@@ -10,22 +10,41 @@ class MovieDetail extends StatelessWidget {
     double height = MediaQuery.of(context).size.height;
     String path;
     path = imgPath + movie.posterPath;
-      return Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text(movie.title),
+        backgroundColor: Colors.transparent,
+        iconTheme: const IconThemeData(color: Colors.white),
+        elevation: 0,
+        title: Text(
+          movie.title,
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
       ),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
             children: <Widget>[
               Container(
-                  padding: EdgeInsets.all(16),
-                  height: height / 1.5,
-                  child: Image.network(path)),
+                padding: EdgeInsets.all(16),
+                height: height / 1.5,
+                child: Image.network(path),
+              ),
               Container(
                 padding: EdgeInsets.only(left: 16, right: 16),
-                child: Text(movie.overview),
-              )
+                child: Text(
+                  movie.overview,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    height: 1.5,
+                  ),
+                ),
+              ),
             ],
           ),
         ),
