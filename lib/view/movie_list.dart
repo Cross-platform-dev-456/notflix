e68912526199aa217a8c0a303086c1dd73df587c
DannyPoff31 <100453345+DannyPoff31@users.noreply.github.com>
2025-10-14 13:23:10 -0400

    Updating movie_list.dart based on poffd branch
    Moving all of my code to main

--- lib/view/movie_list.dart
+++ lib/view/movie_list.dart
@@ -1,11 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:notflix/util/api.dart';
 import 'movie_detail.dart';
-import 'search.dart';
 
 class MovieList extends StatefulWidget {
-  const MovieList({super.key});
-
   @override
   _MovieListState createState() => _MovieListState();
 }
@@ -15,10 +12,13 @@ class _MovieListState extends State<MovieList> {
   APIRunner? helper;
   int? moviesCount;
   List? movies;
+  List? horror;
+  List? action;
+  List? adventure;
+  List? mystery;
+  List? documentery;
+  List? animation;
 
-  final String iconBase = 'https://image.tmdb.org/t/p/w92/';
-  final String defaultImage =
-      'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
   Icon visibleIcon = Icon(Icons.search);
   Widget searchBar = Text('Movies');
 
@@ -31,52 +31,70 @@ class _MovieListState extends State<MovieList> {
 
   @override
   Widget build(BuildContext context) {
+    final String iconBase = 'https://image.tmdb.org/t/p/w92/';
+    final String defaultImage = 'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
+
     NetworkImage image;
     return Scaffold(
       appBar: AppBar(title: searchBar, actions: <Widget>[
-        IconButton(             // Place Holder for the user home page
-          icon: Icon(Icons.home),
-          onPressed: () {
-            initialize(); 
-          }
-        ), 
         IconButton(
           icon: visibleIcon,
           onPressed: () {
-            MaterialPageRoute route = MaterialPageRoute(      // Routes to the search page
-              builder: (_) => Search());
-            Navigator.push(context, route);
+            setState(
+              () {
+                if (this.visibleIcon.icon == Icons.search) {
+                  this.visibleIcon = Icon(Icons.cancel);
+                  this.searchBar = TextField(
+                    textInputAction: TextInputAction.search,
+                    onSubmitted: (String text) {
+                      search(text);
+                    },
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                    ),
+                  );
+                } else {
+                  this.visibleIcon = Icon(Icons.search);
+                  this.searchBar = Text('Notflix');
+                }
+              },
+            );
           },
         ),
-      ]),
-      body: ListView.builder(
-        itemCount: (moviesCount == null) ? 0 : moviesCount,
-        itemBuilder: (BuildContext context, int position) {
-          if (movies?[position].posterPath != null) {
-            image = NetworkImage(iconBase + movies?[position].posterPath);
-          } else {
-            image = NetworkImage(defaultImage);
-          }
-          return Card(
-            color: Colors.white,
-            elevation: 2.0,
-            child: ListTile(
-              onTap: () {
-                MaterialPageRoute route = MaterialPageRoute(
-                    builder: (_) => MovieDetail(movies?[position]));
-                Navigator.push(context, route);
-              },
-              leading: CircleAvatar(
-                backgroundImage: image,
-              ),
-              title: Text(movies?[position].title),
-              subtitle: Text('${'Released: ' +
-                  movies?[position].releaseDate} - Vote: ${movies![position].voteAverage}'),
-            ),
-          );
-        },
-      ),
-    );
+      ]),      
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: movieTitle(title: 'Upcoming')),
+          SliverToBoxAdapter(
+            child: movieGroup(moviesCount: moviesCount, movieGroup: movies)
+          ),
+          SliverToBoxAdapter(child: movieTitle(title: 'Action')),
+          SliverToBoxAdapter(
+            child: movieGroup(moviesCount: moviesCount, movieGroup: action)
+          ),
+          SliverToBoxAdapter(child: movieTitle(title: 'Adventure')),
+          SliverToBoxAdapter(
+            child: movieGroup(moviesCount: moviesCount, movieGroup: adventure)
+          ),
+          SliverToBoxAdapter(child: movieTitle(title: 'Horror')),
+          SliverToBoxAdapter(
+            child: movieGroup(moviesCount: moviesCount, movieGroup: horror)
+          ),
+          SliverToBoxAdapter(child: movieTitle(title: 'Mystery')),
+          SliverToBoxAdapter(
+            child: movieGroup(moviesCount: moviesCount, movieGroup: mystery)
+          ),
+          SliverToBoxAdapter(child: movieTitle(title: 'Animation')),
+          SliverToBoxAdapter(
+            child: movieGroup(moviesCount: moviesCount, movieGroup: animation)
+          ),
+          SliverToBoxAdapter(child: movieTitle(title: 'Documentery')),
+          SliverToBoxAdapter(
+            child: movieGroup(moviesCount: moviesCount, movieGroup: documentery)
+          ),
+        ]),
+      );
   }
 
   Future search(text) async {
@@ -91,11 +109,77 @@ class _MovieListState extends State<MovieList> {
 
   Future initialize() async {
     movies = (await helper?.getUpcoming())!;
+    horror = (await helper?.getGenre('Horror'))!;
+    action = (await helper?.getGenre('Action'))!;
+    adventure = (await helper?.getGenre('Adventure'))!;
+    mystery = (await helper?.getGenre('Mystery'))!;
+    documentery = (await helper?.getGenre('Documentary'))!;
+    animation = (await helper?.getGenre('Animation'))!;
     setState(
       () {
         moviesCount = movies?.length;
         movies = movies;
+        horror = horror;
+        action = action;
+        adventure = adventure;
+        mystery = mystery;
+        documentery = documentery;
+        animation = animation;
       },
     );
   }
+}
+
+// widget for showing the movie group title
+Widget movieTitle({required title}) {
+  return Padding(padding: const EdgeInsets.all(8),
+    child: Text('$title', style: TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.bold,
+      )
+    )
+  );
+}
+
+// widget for making a horizontally scrollable movie list
+Widget movieGroup({required moviesCount, required movieGroup, }) {
+  final String iconBase = 'https://image.tmdb.org/t/p/w92/';
+  final String defaultImage = 'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
+  
+  NetworkImage image;
+
+  return Container(
+    height: 100, 
+    child: ListView.builder(
+      itemCount: (moviesCount == null) ? 0 : moviesCount,
+      scrollDirection: Axis.horizontal,
+      itemBuilder: (BuildContext context, int position) {
+        if (movieGroup?[position].posterPath != null) {
+          image = NetworkImage(iconBase + movieGroup?[position].posterPath);
+        } else {
+          image = NetworkImage(defaultImage);
+        }
+        return Container(
+          color: Colors.white,
+          width: 200,
+          height: 10,
+          child: ListTile(
+            onTap: () {
+              MaterialPageRoute route = MaterialPageRoute(
+                  builder: (_) => MovieDetail(movieGroup?[position]));
+              Navigator.push(context, route);
+            },
+            leading: CircleAvatar(
+              backgroundImage: image,
+            ),
+            title: Text(movieGroup?[position].title),
+            subtitle: Text('Released: ' +
+                movieGroup?[position].releaseDate +
+                ' - Vote: ' +
+                movieGroup![position].voteAverage.toString()),
+          ),
+        );
+      }
+    )
+  );
 }
